562d3f414c68a8729ee8ea97f588b61d698787e2
Akira <junrongfu6@gmail.com>
2023-08-27 16:00:44 +0800

    to add comments to database

--- app/routes/movie.$id.comments.tsx
+++ app/routes/movie.$id.comments.tsx
@@ -1,5 +1,5 @@
 import { ActionArgs, LoaderArgs, json } from "@remix-run/node";
-import { Form, useLoaderData, useParams } from "@remix-run/react";
+import { Form, useLoaderData, useNavigation, useParams } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 
 export async function loader({params}:LoaderArgs){
@@ -14,21 +14,31 @@ export async function loader({params}:LoaderArgs){
     return json({data})
 }
 
-export async function action({}:ActionArgs){
-    
+export async function action({request}:ActionArgs){
+    const formData = await request.formData()
+    const data = await db.comment.create({
+        data:{
+            message:formData.get('comment') as string,
+            movieId: formData.get('id') as string
+        }
+    })
+
+    return json({data})
 }
 
 export default function Comments(){
     const {id} = useParams()
     const {data} = useLoaderData<typeof loader>()
+    const navigation = useNavigation()
+
     return (
         <div className="rounded-lg border p-3">
             <h2 className="text-xl font-semibold mb-5">Your Opinion</h2>
             <div>
-                <Form>
+                <Form method="POST">
                     <textarea name="comment" className="w-full border border-teal-500 rounded-lg p-2"></textarea>
                     <input type="hidden" name="id" value={id} />
-                    <button type="submit" className="bg-teal-500 px-4 py-2 rounded-lg text-white">Submit</button>
+                    {navigation.state === 'submitting' ? (<button type="button" disabled className="bg-teal-500 px-4 py-2 rounded-lg text-white">Loading...</button>) :(<button type="submit" className="bg-teal-500 px-4 py-2 rounded-lg text-white">Add Comment</button>)}
                 </Form>
                 <div className="mt-5 flex flex-col gap-y-3">
                     {data.map((post) =>(
